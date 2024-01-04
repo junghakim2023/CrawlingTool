bfe640679834a4cf93fc9856dda0a6aed63acc51
junghakim2023 <jungha.kim.2023@gmail.com>
2024-01-04 14:52:16 -0800

    Feature : init excel manager

---
+++ CrawlingTool/CrawlingTool/ExcelManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace CrawlingTool
+{
+    class ExcelManager
+    {
+        IXLWorksheet worksheet;
+        public ExcelManager(String fileName, int sheetNumber)
+        {
+            var workbook = new XLWorkbook(fileName);
+            worksheet = workbook.Worksheet(sheetNumber);
+        }
+
+        public String ReadExcel(int row, int column)
+        {
+            var rowData = worksheet.Row(row);
+            var cell = rowData.Cell(column);
+            String value = cell.GetValue<String>();
+            return value;
+        }
+
+        public int GetRowCount()
+        {
+            return worksheet.LastRowUsed().RowNumber();
+        }
+    }
+}
