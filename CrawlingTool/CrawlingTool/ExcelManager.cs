062407324c6e53552b6e98c249be02b303e06404
junghakim2023 <jungha.kim.2023@gmail.com>
2024-01-05 14:02:28 -0800

    Feature : excel - excel data to list

--- CrawlingTool/CrawlingTool/ExcelManager.cs
+++ CrawlingTool/CrawlingTool/ExcelManager.cs
@@ -4,29 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using OpenQA.Selenium;
 
 namespace CrawlingTool
 {
-    class ExcelManager
-    {
+    class ExcelManager{
         IXLWorksheet worksheet;
-        public ExcelManager(String fileName, int sheetNumber)
-        {
+        public ExcelManager(String fileName, int sheetNumber){
             var workbook = new XLWorkbook(fileName);
             worksheet = workbook.Worksheet(sheetNumber);
         }
 
-        public String ReadExcel(int row, int column)
-        {
+        public String ReadExcel(int row, int column){
             var rowData = worksheet.Row(row);
             var cell = rowData.Cell(column);
             String value = cell.GetValue<String>();
             return value;
         }
 
-        public int GetRowCount()
-        {
+        public int GetRowCount(){
             return worksheet.LastRowUsed().RowNumber();
+        }
+
+        public void ExcelDataToList(FileInfo file){
+            List<String> list = new List<String>();
+            String fileName = file.FullName;              
+            var excel = new ExcelManager(fileName, 1);
+
+            int rowCount = excel.GetRowCount();
+            for (int i = 2; i <= rowCount; i++){
+                for (int j = 1; j < 2; j++) {
+                    var info = excel.ReadExcel(i, 1);
+                    list.Add(info);
+                }
+               
+            }
         }
     }
 }
